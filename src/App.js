7b696147f9b31e185e45441e7b047c4a84c03d43
twigNLP <jowayholm@google.com>
2021-07-23 10:05:47 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers =[
+{
+    'id': 1,
+    'img': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '1234',
+    'gender': '남자',
+    'job': '대학생'
+},
+{
+    'id': 2,
+    'img': 'https://placeimg.com/64/64/2',
+    'name': '홍길동1',
+    'birthday': '4567',
+    'gender': '남자',
+    'job': '대학생'
+},
+{
+    'id': 1,
+    'img': 'https://placeimg.com/64/64/any3',
+    'name': '홍길동2',
+    'birthday': '8901',
+    'gender': '남자',
+    'job': '대학생'
 }
-
+]
+class App extends Component {
+    render() {
+        return (
+        <div>
+        {
+            customers.map(c => {
+                return(
+                    <Customer
+                        key={c.id}
+                        id = {c.id}
+                        img={c.img}
+                        name={c.name} 
+                        birthday={c.birthday} 
+                        gender={c.gender} 
+                        job={c.job}/>
+                );
+            })
+        }
+       </div>  
+        );
+    }
+}
 export default App;
